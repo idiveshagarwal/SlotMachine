fca3f19047ca108658ad0d5d1c5058584a86f0a7
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-06-12 10:00:33 +0530

    SlotMachine

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.css'
+import SlotM from './SlotMachine'
+
+const App = () => {
+	return ( < >
+		<
+		h1 className = "heading_style" > Welcome to Slot🎰 Machine Game < /h1> <
+		SlotM x = "🍧"
+		y = "🍧"
+		z = "🍧" / >
+		<
+		SlotM x = " 😃 "
+		y = " 😉"
+		z = " 😃" / >
+		<
+		SlotM x = "🍎"
+		y = "🍎"
+		z = "🍎" / >
+		<
+		SlotM x = "🙈"
+		y = "🙊"
+		z = "🙉" / >
+		<
+		SlotM x = "🎧"
+		y = "🍎"
+		z = "🎧" / >
+		<
+		/>);
+	};
+}
+
+export default App;
